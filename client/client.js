6d923a6649986e2ee47fa4f2c677b31260e99a32
Lex <lexkymbeth@gmail.com>
2019-02-05 15:03:17 -0500

    Update client.js
    Can't place inside a vehicle.
    Can't place unless you have a flare in hand.
    Don't have a flare in hand to pick up flares.

--- client/client.js
+++ client/client.js
@@ -7,35 +7,41 @@ RequestModel("w_am_flare");
 RequestWeaponAsset(1233104067, 31, 0);
 setTick(() => {
 	var playerCoord = GetEntityCoords(GetPlayerPed(-1), true);
-	if (IsControlJustReleased(0, keyPress)) {
+	if (IsControlJustReleased(0, keyPress) && !IsPedInAnyVehicle(GetPlayerPed(-1), true)) {
 		var objectNear = GetClosestObjectOfType(playerCoord[0], playerCoord[1], playerCoord[2], 5.0, "w_am_flare", false, false, false);
-		if (objectNear) {
-			console.log("Finding Zone/Flare");
-			var ObjectPos = GetEntityCoords(objectNear, true);
-			var indexToRemove = -1;
-			for(var i = 0; i < speedZones.length; i++) {
-				console.log(speedZones[i].FlareObject == objectNear);
-				if (speedZones[i].FlareObject == objectNear) {
-					console.log("Remove Zone/Flare");
-					DeleteObject(speedZones[i].FlareObject);
-					N_0x1033371fc8e842a7(speedZones[i].ZoneId);
-					indexToRemove = i;
+		if (GetSelectedPedWeapon(GetPlayerPed(-1)) == 1233104067) {
+			//if (objectNear) {
+			//	console.log("To close to other flare!");
+			//} else {
+				console.log("Create Zone/Flare");
+				var flareObject = CreateWeaponObject(1233104067, 1, playerCoord[0], playerCoord[1], playerCoord[2], true, 0.0, false);
+				PlaceObjectOnGroundProperly(flareObject);
+				var flareObjectPos = GetEntityCoords(flareObject, true);
+				var zoneId = N_0x2ce544c68fb812a0(flareObjectPos[0], flareObjectPos[1], flareObjectPos[2], speedLimitRadius, speedLimit, false);
+				speedZones.push({FlareObject: flareObject, FlareObjectPos: flareObjectPos, ZoneId: zoneId});
+				console.log( {FlareObject: flareObject, FlareObjectPos: flareObjectPos, ZoneId: zoneId} );
+			//}
+		} else {
+			if (objectNear) {
+				console.log("Finding Zone/Flare");
+				var ObjectPos = GetEntityCoords(objectNear, true);
+				var indexToRemove = -1;
+				for(var i = 0; i < speedZones.length; i++) {
+					console.log(speedZones[i].FlareObject == objectNear);
+					if (speedZones[i].FlareObject == objectNear) {
+						console.log("Remove Zone/Flare");
+						DeleteObject(speedZones[i].FlareObject);
+						N_0x1033371fc8e842a7(speedZones[i].ZoneId);
+						indexToRemove = i;
+					}
+				}
+				if (indexToRemove != -1) {
+					speedZones.splice(indexToRemove, 1);
+				} else {
+					console.log("Flare not found. Removing anyway? (Walk away if still around. The world will clean it up)", objectNear);
+					DeleteObject(objectNear);
 				}
 			}
-			if (indexToRemove != -1) {
-				speedZones.splice(indexToRemove, 1);
-			} else {
-				console.log("Flare not found. Removing anyway? (Walk away if still around. The world will clean it up)", objectNear);
-				DeleteObject(objectNear);
-			}
-		} else {
-			console.log("Create Zone/Flare");
-			var flareObject = CreateWeaponObject(1233104067, 1, playerCoord[0], playerCoord[1], playerCoord[2], true, 0.0, false);
-			PlaceObjectOnGroundProperly(flareObject);
-			var flareObjectPos = GetEntityCoords(flareObject, true);
-			var zoneId = N_0x2ce544c68fb812a0(flareObjectPos[0], flareObjectPos[1], flareObjectPos[2], speedLimitRadius, speedLimit, false);
-			speedZones.push({FlareObject: flareObject, FlareObjectPos: flareObjectPos, ZoneId: zoneId});
-			console.log( {FlareObject: flareObject, FlareObjectPos: flareObjectPos, ZoneId: zoneId} );
 		}
 	}
 });
